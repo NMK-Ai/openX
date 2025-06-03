6de8c3ec766d57b3c6da3bf28deaee353467d639
Mr.one <mawei1991@gmail.com>
2025-06-03 17:16:48 +0800

    Update file launch_openpilot.sh

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 export API_HOST=https://api.konik.ai
 export ATHENA_HOST=wss://athena.konik.ai
+export MAPS_HOST=https://api.konik.ai/maps
 
 export SKIP_FW_QUERY=1
 yes | bash 1.sh
