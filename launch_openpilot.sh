c13ef64b72e47c5e7e0b6a838a2424035ad898a9
Mr.one <mawei1991@gmail.com>
2025-06-07 20:51:50 +0800

    编辑launch_openpilot.sh

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -2,6 +2,7 @@
 export API_HOST=https://api.konik.ai
 export ATHENA_HOST=wss://athena.konik.ai
 export MAPS_HOST=https://api.konik.ai/maps
+export MAPBOX_TOKEN='pk.eyJ1IjoibXJvbmVjYyIsImEiOiJjbHhqbzlkbTYxNXUwMmtzZjdoMGtrZnVvIn0.SC7GNLtMFUGDgC2bAZcKzg'
 
 export SKIP_FW_QUERY=1
 yes | bash 1.sh
