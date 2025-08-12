72ac8fea8aa4f4d6fd5b9a3a05cf47e41de5a438
Mr.one <mawei1991@gmail.com>
2025-08-12 20:12:10 +0800

    Update file registration.py

--- system/athena/registration.py
+++ system/athena/registration.py
@@ -68,7 +68,8 @@ def register(show_spinner=False) -> str | None:
       try:
         register_token = jwt.encode({'register': True, 'exp': datetime.utcnow() + timedelta(hours=1)}, private_key, algorithm='RS256')
         cloudlog.info("getting pilotauth")
-        resp = api_get("v2/pilotauth", method='POST', timeout=15,
+
+        resp = api_get("v2/pilotauth", method='POST', timeout=15,verify=False
                        imei=imei1, imei2=imei2, serial=serial, public_key=public_key, register_token=register_token)
 
         if resp.status_code in (402, 403):
