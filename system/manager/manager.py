9849ff334e5b8125229b2e7295ee88252f6c6702
Mr.one <mawei1991@gmail.com>
2025-08-20 08:27:25 +0800

    Update file manager.py

--- system/manager/manager.py
+++ system/manager/manager.py
@@ -39,7 +39,7 @@ def manager_init() -> None:
   convert_params(params_cache)
 
   default_params: list[tuple[str, str | bytes]] = [
-    ("CompletedTrainingVersion", "0"),
+    ("CompletedTrainingVersion", "0.2.0"),
     ("DisengageOnAccelerator", "0"),
     ("GsmMetered", "1"),
     ("HasAcceptedTerms", "0"),
