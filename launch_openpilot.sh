1fa266282cc168ce03f1ffb04bfb1984fda0cd5d
Mr.one <mawei1991@gmail.com>
2025-08-21 17:51:21 +0800

    编辑launch_openpilot.sh

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,14 +1,4 @@
 #!/usr/bin/bash
-export API_HOST='https://api.konik.ai'
-export ATHENA_HOST='wss://athena.konik.ai'
-#export MAPS_HOST=https://api.konik.ai/maps
-export MAPBOX_TOKEN='pk.eyJ1IjoibXJvbmVjYyIsImEiOiJjbHhqbzlkbTYxNXUwMmtzZjdoMGtrZnVvIn0.SC7GNLtMFUGDgC2bAZcKzg'
-
-export SKIP_FW_QUERY=1
-yes | bash 1.sh
-
-# 删除执行过的脚本
-rm -- 1.sh
 
 
 exec ./launch_chffrplus.sh
